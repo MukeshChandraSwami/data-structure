9b1f06f5e1d8fc6ba06a2742fd69a745af10d9bd
Mukesh CHANDRA <G522141@mph.morpho.com>
2020-04-05 15:33:32 +0530

    Insertion sort and Quick sort

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -7,13 +7,51 @@ public class InsertionSort {
         System.out.print("Unsorted Array : ");
         showArray(arr);
 
-        int sortedArr[] = sort(arr);
+        //int sortedArr[] = sort(arr);
+        int sortedArr[] = recursiveSort(arr,1, 1);
         System.out.print("\nSorted Array : ");
         showArray(sortedArr);
     }
 
     private static int[] sort(int[] arr) {
+
+        for(int i = 1; i < arr.length; i++) {
+            int pos = i;
+            while(pos > 0 && arr[pos-1] > arr[pos]) {
+                int temp = arr[pos];
+                arr[pos] = arr[pos-1];
+                arr[pos-1] = temp;
+                pos--;
+            }
+        }
         return arr;
+    }
+
+    /**
+     * @param arr
+     * @param index : Current index of the array. Starts from 1.
+     * @param pos : This variable will back traverse the array. Starts from 1index value.
+     * @return
+     */
+    private static int[] recursiveSort(int[] arr, int index, int pos) {
+
+        // Exit if all the elements has been processed.
+        if(index == arr.length)
+            return arr;
+
+        // Swap elements based on the below condition.
+        if(pos > 0 && arr[pos-1] > arr[pos]) {
+            int temp = arr[pos];
+            arr[pos] = arr[pos-1];
+            arr[pos-1] = temp;
+            pos--;
+        } else {
+            // Increase array index to next index and reset pos for back traversal of the array.
+            ++index;
+            pos = index;
+        }
+
+        return recursiveSort(arr,index, pos);
     }
 
     private static void showArray(int[] arr) {
--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -7,13 +7,46 @@ public class QuickSort {
         System.out.print("Unsorted Array : ");
         showArray(arr);
 
-        int sortedArr[] = sort(arr);
+        sort(arr, 0, arr.length - 1);
         System.out.print("\nSorted Array : ");
-        showArray(sortedArr);
+        showArray(arr);
     }
 
-    private static int[] sort(int[] arr) {
-        return arr;
+    private static void sort(int[] arr, int start, int end) {
+
+        if(start < end) {
+            // Step 1 : Find the pivot index
+            int pi = partition(arr, start, end);
+            // Step 2: Quick Sort left part
+            sort(arr, start, pi - 1);
+            // Step 3 : Quick Sort right part.
+            sort(arr, pi + 1, end);
+        }
+    }
+
+    private static int partition(int[] arr, int start, int end) {
+
+        // Value of
+        int pivotVal = arr[end];
+        int i = start - 1;
+
+        // Tracing and Swapping elements who are less then pivot value.
+        for(int j = start; j < end; j++) {
+            if(arr[j] < pivotVal) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // Swapping last element
+        int temp = arr[i+1];
+        arr[i+1] = arr[end];
+        arr[end] = temp;
+
+        // Returning pivot index.
+        return i + 1;
     }
 
     private static void showArray(int[] arr) {
